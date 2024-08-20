8e20525435780577b2c62f5f0d3b3851d1f6b059
jeanFrancoGuerreroAcero <guerreroacerojeanfranco>
2024-08-20 12:58:24 -0500

    fixed :sparkles: initial deveploment project

---
+++ script.js
@@ -0,0 +1,356 @@
+var json = [
+    {
+        "id": 1,
+        "grupo": "T1",
+        "postulados": [
+            {
+                "id": 1,
+                "cedula": "10001",
+                "nombre": "jerxon",
+                "apellido": "correa",
+                "direccion": "calle 1 #2",
+                "acudiente": "maria lizaraso",
+                "telefono": "3112812609",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T1"
+            },
+            {
+                "id": 2,
+                "cedula": "10002",
+                "nombre": "Enrique",
+                "apellido": "Mejia",
+                "direccion": "call 1 #3",
+                "acudiente": "eduardo guerrero",
+                "telefono": "3188653147",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T1"
+            },
+            {
+                "id": 3,
+                "cedula": "10003",
+                "nombre": "Carolina",
+                "apellido": "Ruiz",
+                "direccion": "Calle 1 #4",
+                "acudiente": "Jose Vidal",
+                "telefono": "3866060",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T1"
+            },
+            {
+                "id": 4,
+                "cedula": "10004",
+                "nombre": "Tomas",
+                "apellido": "Canto",
+                "direccion": "Calle 1 #5",
+                "acudiente": "Maria Bendeck",
+                "telefono": "3741504",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T1"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "grupo": "M1",
+        "estudiantes": [
+            {
+                "id": 1,
+                "cedula": "20001",
+                "nombre": "Juan",
+                "apellido": "Alarcon",
+                "direccion": "call 2 #3",
+                "acudiente": "camila Alarcon",
+                "telefono": "380009",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "M1"
+            },
+            {
+                "id": 2,
+                "cedula": "20002",
+                "nombre": "Carlos",
+                "apellido": "Quintana",
+                "direccion": "Calle 2 #4",
+                "acudiente": "Ximena Tobar",
+                "telefono": "364478841",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "M1"
+            },
+            {
+                "id": 3,
+                "cedula": "20003",
+                "nombre": "Julio",
+                "apellido": "Flores",
+                "direccion": "Calle 2 #5",
+                "acudiente": "Aleja Torres",
+                "telefono": "35789418",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "6-10",
+                "grupo": "M1"
+            },
+            {
+                "id": 4,
+                "cedula": "20004",
+                "nombre": "Pedro",
+                "apellido": "Diaz",
+                "direccion": "Calle 2 #6",
+                "acudiente": "Elena Lopez",
+                "telefono": "32654879",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "M1"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "grupo": "P1",
+        "estudiantes": [
+            {
+                "id": 1,
+                "cedula": "30001",
+                "nombre": "Teresa",
+                "apellido": "Gomez",
+                "direccion": "Calle 3 #4",
+                "acudiente": "Cecilia Cardona",
+                "telefono": "3182687498604",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "P1"
+            },
+            {
+                "id": 2,
+                "cedula": "30002",
+                "nombre": "Mauricio",
+                "apellido": "Zambrano",
+                "direccion": "Calle 3 #5",
+                "acudiente": "Andres Munoz",
+                "telefono": "37768082",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "P1"
+            },
+            {
+                "id": 3,
+                "cedula": "30003",
+                "nombre": "Martin",
+                "apellido": "Pantoja",
+                "direccion": "Calle 3 #6",
+                "acudiente": "Yenny Castillo",
+                "telefono": "3157697639",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "P1"
+            },
+            {
+                "id": 4,
+                "cedula": "30004",
+                "nombre": "Christian",
+                "apellido": "Maldonado",
+                "direccion": "Calle 3 #7",
+                "acudiente": "Luz Castillo",
+                "telefono": "3112947401",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "P1"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "grupo": "T2",
+        "estudiantes": [
+            {
+                "id": 1,
+                "cedula": "40001",
+                "nombre": "Yaned",
+                "apellido": "Rojas",
+                "direccion": "Calle 4 #5",
+                "acudiente": "Maria Contreras",
+                "telefono": "3137844603",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T2"
+            },
+            {
+                "id": 2,
+                "cedula": "40002",
+                "nombre": "Luz",
+                "apellido": "Piso",
+                "direccion": "Calle 4 #6",
+                "acudiente": "Alexander Jaramillo",
+                "telefono": "345789621",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T2"
+            },
+            {
+                "id": 3,
+                "cedula": "40003",
+                "nombre": "Maria",
+                "apellido": "Bolaños",
+                "direccion": "Calle 4 #7",
+                "acudiente": "Juan Gutierrez",
+                "telefono": "356487921",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T2"
+            },
+            {
+                "id": 4,
+                "cedula": "40004",
+                "nombre": "Yury",
+                "apellido": "Tobar",
+                "direccion": "Calle 4 #8",
+                "acudiente": "Esmith Moncada",
+                "telefono": "369587425",
+                "estado": "",
+                "salon": "",
+                "trainer": "",
+                "riesgo": "",
+                "ruta": "",
+                "fechaIni": "12-03-2024",
+                "fechaFin": "13-04-2024",
+                "ocupado": "",
+                "grupo": "T2"
+            }
+        ]
+    }
+];
+//let nuevoArchivo = json.push({"nombre":"jean franco","apellido":"Guerrero Acerooooooooooooo"})
+
+console.log("----------------------------");
+console.log("BIENVENIDO A CAMPUSLANDS ERP");
+console.log("----------------------------");
+console.log("")
+console.log("CUAL ES TU ROL");
+console.log("--------------");
+console.log("1. coordinador")
+console.log("2. camper")
+var rol=prompt();
+var booleanito = true;
+
+while(booleanito){
+    console.clear();
+    if (rol=="1"){
+        console.log("============================");
+        console.log("   BIENVENIDO COORDINADOR   ");
+        console.log("============================"); 
+        console.log("1. Inscribir postulados");
+        console.log("2. Ingresar nota a campers que finalizaron modulo"); 
+        console.log("3. Campers en peligro");
+        console.log("4. volver al menu principal")
+    QueDesea=prompt("que funcion vas a realizar:");
+        console.clear()
+        if (QueDesea=="1"){
+            for (var i of json[0]["postulados"]){
+                console.log("ID:",i["id"],i["nombre"])
+                
+            }
+
+        }
+
+
+        console.log("hola")
+        break
+    }
+    if (rol=="2"){
+        console.log("chao")
+        break
+    }
+}
